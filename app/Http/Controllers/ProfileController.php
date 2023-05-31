a32d5ed57b489a520a492acf17186b9edb4cb352
unknown <qory1022@gmail.com>
2023-05-31 19:59:58 +0700

    upload baru

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $pageTitle = 'Profile';
+
+        return view('profile', ['pageTitle' => $pageTitle]);
+    }
+
+    public function index()
+    {
+        $pageTitle = 'Profile';
+
+        return view('profile', ['pageTitle' => $pageTitle]);
+    }
+}
